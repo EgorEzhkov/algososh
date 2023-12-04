9422cd08c74ae12aa92341dd7a2eb43d6f716c54
Egor Ezhkov <egorkashpak21@yandex.ru>
2023-12-04 22:53:54 +0300

    radioInput :D

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Direction } from "../../types/direction";
 import { Button } from "../ui/button/button";
 import { RadioInput } from "../ui/radio-input/radio-input";
@@ -6,15 +6,29 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from './sorting-page.module.css'
 
 export const SortingPage: React.FC = () => {
+
+  const [radioSelect, setRadioSelect] = useState("Выбор")
+
+  const onOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRadioSelect(e.target.value)
+  }
+
+
+  const onChangeRadio = (radio: boolean, setRadio: React.Dispatch<React.SetStateAction<boolean>>) => {
+    setRadio(!radio)
+
+  }
+
+
+
   return (
     <SolutionLayout title="Сортировка массива">
       <main className={styles.main}>
         <div className={styles.settingsContainer}>
           <div className={styles.radioInput}>
-
-            <RadioInput label="Выбор" checked={false}></RadioInput>
-            <RadioInput label="Массив"></RadioInput>
-
+            <RadioInput label="Выбор" name="sort" value='Выбор' onChange={onOptionChange} defaultChecked></RadioInput>
+            <RadioInput label="Пузырёк" name='sort' value='Пузырёк' onChange={onOptionChange} ></RadioInput>
+            {console.log(radioSelect)}
           </div>
           <div className={styles.typeSortContainer}>
             <Button text="По возрастанию" sorting={Direction.Ascending} extraClass={styles.btn}></Button>
